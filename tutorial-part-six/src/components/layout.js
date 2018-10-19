82550cfea40a2803f8b65e8ab0542d1673b25121
Wes Cutting <wesleycutting@gmail.com>
2018-10-19 16:38:54 -0600

    Completed Tutorials 5 & 6

---
+++ tutorial-part-six/src/components/layout.js
@@ -0,0 +1,54 @@
+import React from "react"
+import { css } from "react-emotion"
+import { StaticQuery, Link, graphql } from "gatsby"
+
+import { rhythm } from "../utils/typography"
+
+export default ({ children }) => (
+    <StaticQuery
+        query={graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `
+        }
+        render={data => (
+            <div
+                className={css`
+          margin: 0 auto;
+          max-width: 700px;
+          padding: ${rhythm(2)};
+          padding-top: ${rhythm(1.5)};
+        `}
+            >
+                <Link to={`/`}>
+                    <h3
+                        className={css`
+              margin-bottom: ${rhythm(2)};
+              display: inline-block;
+              font-style: normal;
+            `}
+                    >
+                        {data.site.siteMetadata.title}
+                    </h3>
+                </Link>
+                
+                <Link
+                    to={`/about/`}
+                    className={css`float: right; margin-left:1em;`}
+                >
+                    About
+                </Link>
+                <Link to={'/my-files'}
+                    className={css`float: right;`}
+                >
+                    Files</Link>
+                {children}
+            </div>
+        )}
+    />
+)
